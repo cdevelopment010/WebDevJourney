cbb185acac314a9be71a004cc8c1ef26ea3901b7
cdevelopment010 <cdevelopment010@gmail.com>
2021-11-30 09:58:43 +0000

    WDJ Main navigation - stopped horizontal, added fontAwesome & close popup

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let popUp = document.getElementById("popUp");
 let closeBtn = document.getElementById("close"); 
 let main = document.getElementById("main"); 
 let header = document.getElementById("header"); 
+let footer = document.getElementById("footer"); 
+let overlay = document.getElementById("overlay"); 
 
 
 disabled.forEach(x => {
@@ -10,11 +12,20 @@ disabled.forEach(x => {
         popUp.style.display = "block"; 
         main.classList.add("blur");
         header.classList.add("blur");
+        footer.classList.add("blur");
+        overlay.style.display = "block"; 
     })
 });
 
-closeBtn.addEventListener("click", function() {
-    popUp.style.display = "none";
-    main.classList.remove("blur");
-    header.classList.remove("blur");
-});
+closeBtn.addEventListener("click", closePopUp);
+overlay.addEventListener("click", closePopUp);
+
+function closePopUp() {
+    if (popUp.style.display == "block") {
+        popUp.style.display = "none";
+        main.classList.remove("blur");
+        header.classList.remove("blur");
+        footer.classList.remove("blur");
+        overlay.style.display = "none"; 
+    }
+}
