57fdb95bf2793f903c1f70ca6df0e0c9686c6f67
cdevelopment010 <cdevelopment010@gmail.com>
2022-01-23 10:52:49 +0000

    WDJ Week 19 - Doggy app update, onclick, styling and html

--- Week19/Sites/doggy-play-date/index.js
+++ Week19/Sites/doggy-play-date/index.js
@@ -1,5 +1,21 @@
 let rejectBtn = document.getElementById('reject-btn'); 
+let acceptBtn = document.getElementById('accept-btn'); 
+let mainCard = document.getElementById('main-card'); 
+let lastCard = document.getElementById('last-card'); 
 
 rejectBtn.addEventListener('click', function() {
-    this.classList.toggle('reject-click');
+    mainCard.classList.toggle('reject-click');
+    intoView();
+    setTimeout(()=>mainCard.classList.toggle('reject-click'),500)
 })
+acceptBtn.addEventListener('click', function() {
+    mainCard.classList.toggle('accept-click');
+    intoView();
+    setTimeout(()=>mainCard.classList.toggle('accept-click'),500); 
+})
+
+function intoView() {
+    lastCard.classList.toggle('intoView-click'); 
+    setTimeout(()=>lastCard.classList.toggle('intoView-click'),500)
+
+}
