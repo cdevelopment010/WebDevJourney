0150c8e9817ff06d247c1b5e1c5ac5af795c3ce5
cdevelopment010 <cdevelopment010@gmail.com>
2022-01-24 10:16:57 +0000

    WDJ Week 20 - Dogs json file and updates to style & js

--- Week19/Sites/doggy-play-date/index.js
+++ Week19/Sites/doggy-play-date/index.js
@@ -2,20 +2,76 @@ let rejectBtn = document.getElementById('reject-btn');
 let acceptBtn = document.getElementById('accept-btn'); 
 let mainCard = document.getElementById('main-card'); 
 let lastCard = document.getElementById('last-card'); 
+let section = document.getElementById('section'); 
+
+let dogImg = document.getElementById('dog-img'); 
+let dogName = document.getElementById('name');
+let dogAge = document.getElementById('age');
+let dogInterest = document.getElementById('interest-text');
+let dogAbout = document.getElementById('about-text');
+
+
+let index = 0; 
+let Dogs =[]
+
+
+fetchDogs();
 
 rejectBtn.addEventListener('click', function() {
     mainCard.classList.toggle('reject-click');
     intoView();
-    setTimeout(()=>mainCard.classList.toggle('reject-click'),500)
+    setTimeout(()=>mainCard.classList.toggle('reject-click'),500);
+    // index = ++index % Dogs.length;
+    ++index;
+    checkDogs(index); 
+    updateDog(index); 
+
 })
 acceptBtn.addEventListener('click', function() {
     mainCard.classList.toggle('accept-click');
     intoView();
     setTimeout(()=>mainCard.classList.toggle('accept-click'),500); 
+    // index = ++index % Dogs.length;
+    ++index;
+    checkDogs(index); 
+    updateDog(index); 
 })
+
+
+
+
+function checkDogs(index) {
+    if (index >= Dogs.length) {
+        section.innerHTML = ''; 
+        section.innerHTML = `
+            <h1 class='title'>Sorry, there are no more dogs in your area!</h1>
+        `
+    }
+}
 
 function intoView() {
     lastCard.classList.toggle('intoView-click'); 
     setTimeout(()=>lastCard.classList.toggle('intoView-click'),500)
 
+}
+
+
+function updateDog(index) {
+    console.log(Dogs[index]['img'])
+    dogImg.src = Dogs[index]['img']; 
+    dogName.innerHTML = Dogs[index]['name']; 
+    dogAge.innerHTML = Dogs[index]['age']; 
+    dogInterest.innerHTML = Dogs[index]['interest']; 
+    dogAbout.innerHTML = Dogs[index]['about']; 
+}
+
+async function fetchDogs (){
+    const response = await fetch('./Dogs.json')
+    .then(res => res.json())
+    .then(data => data.forEach(x=> {
+        Dogs.push(x)
+    })); 
+
+
+    updateDog(index); 
 }
