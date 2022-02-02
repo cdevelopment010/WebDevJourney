bd9c675850de4e8d267273f5a388647dfb8b1105
cdevelopment010 <cdevelopment010@gmail.com>
2022-02-02 14:40:12 +0000

    WDJ Week 21 - adding to-do-list, a goal setting site

---
+++ Week21/Sites/to-do-list/index.js
@@ -0,0 +1,63 @@
+const listGroup = document.getElementById('goals'); 
+const goalTitleInput = document.getElementById('goal-item');
+const goalWhyInput = document.getElementById('goal-why');
+const btn = document.getElementById('add-goal-btn');
+let clickCount = 0; 
+
+btn.addEventListener('click', addGoal); 
+
+
+
+function addGoal() {
+    console.log("clicked"); 
+    let goalTitle = String(goalTitleInput.value); 
+    let why = String(goalWhyInput.value); 
+
+    let li = document.createElement('li'); 
+    let divTitle = document.createElement('div'); 
+    let divWhy = document.createElement('div'); 
+    let caret = document.createElement('i'); 
+
+
+    caret.className = 'fas fa-caret-down'; 
+    caret.addEventListener('click', accordian); 
+
+    divTitle.className = 'goal-title'; 
+    divTitle.innerText = goalTitle;
+    divTitle.appendChild(caret);
+    
+    divWhy.className = 'why'; 
+    divWhy.innerText = why;
+    
+    li.addEventListener('click', removeGoal); 
+    li.className = 'card'; 
+    li.appendChild(divTitle); 
+    li.appendChild(divWhy); 
+
+   
+
+    listGroup.appendChild(li); 
+
+    goalTitleInput.value = ''; 
+    goalWhyInput.value = ''; 
+
+}
+
+function accordian() {
+    this.parentElement.parentElement.querySelector('.why').classList.toggle('why-active'); 
+    this.classList.toggle('i-active');
+}
+
+function removeGoal() {
+    clickCount++ 
+        if(clickCount == 1) {
+            singleClickTimer = setTimeout(function() {
+                clickCount = 0; 
+            }, 300 )
+        } else if (clickCount == 2) {
+            clearTimeout(singleClickTimer); 
+            clickCount = 0; 
+            console.log('removeGoal clicked')
+            this.remove(); 
+        }
+}
