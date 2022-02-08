869ff367ebe4530e5557b75fa941dfaa20ff134d
cdevelopment010 <cdevelopment010@gmail.com>
2022-02-08 09:44:15 +0000

    WDJ Week 22 - add local storage functionality and improving script.

--- Week21/Sites/to-do-list/index.js
+++ Week21/Sites/to-do-list/index.js
@@ -3,40 +3,28 @@ const goalTitleInput = document.getElementById('goal-item');
 const goalWhyInput = document.getElementById('goal-why');
 const btn = document.getElementById('add-goal-btn');
 let clickCount = 0; 
+let goals ={};
 
 btn.addEventListener('click', addGoal); 
 
+if(localStorage.getItem('Goals') !== 'null') {
+    goals = JSON.parse(localStorage.getItem('Goals')); 
+    for (let goal in goals) {
+        createLi(goal, goals[goal]); 
+    }
+
+}
 
 
 function addGoal() {
-    console.log("clicked"); 
     let goalTitle = String(goalTitleInput.value); 
-    let why = String(goalWhyInput.value); 
+    let why = String(goalWhyInput.value);    
+    let currentGoal = {[goalTitle]: why}
 
-    let li = document.createElement('li'); 
-    let divTitle = document.createElement('div'); 
-    let divWhy = document.createElement('div'); 
-    let caret = document.createElement('i'); 
-
-
-    caret.className = 'fas fa-caret-down'; 
-    caret.addEventListener('click', accordian); 
-
-    divTitle.className = 'goal-title'; 
-    divTitle.innerText = goalTitle;
-    divTitle.appendChild(caret);
+    goals = Object.assign({}, goals, currentGoal);    
+    localStorage.setItem('Goals', JSON.stringify(goals))
     
-    divWhy.className = 'why'; 
-    divWhy.innerText = why;
-    
-    li.addEventListener('click', removeGoal); 
-    li.className = 'card'; 
-    li.appendChild(divTitle); 
-    li.appendChild(divWhy); 
-
-   
-
-    listGroup.appendChild(li); 
+    createLi(goalTitle, why); 
 
     goalTitleInput.value = ''; 
     goalWhyInput.value = ''; 
@@ -57,7 +45,34 @@ function removeGoal() {
         } else if (clickCount == 2) {
             clearTimeout(singleClickTimer); 
             clickCount = 0; 
-            console.log('removeGoal clicked')
-            this.remove(); 
+            console.log('removeGoal clicked'); 
+            this.remove();
+            delete goals[this.children[0].innerText]; 
+            localStorage.setItem('Goals', JSON.stringify(goals));            
         }
+}
+
+
+function createLi(goalTitle, why) {
+    let li = document.createElement('li'); 
+    let divTitle = document.createElement('div'); 
+    let divWhy = document.createElement('div'); 
+    let caret = document.createElement('i'); 
+
+    caret.className = 'fas fa-caret-down'; 
+    caret.addEventListener('click', accordian); 
+
+
+    divTitle.className = 'goal-title'; 
+    divTitle.innerText = goalTitle;
+    divTitle.appendChild(caret);
+    
+    divWhy.className = 'why'; 
+    divWhy.innerText = why;
+
+    li.addEventListener('click', removeGoal); 
+    li.className = 'card'; 
+    li.appendChild(divTitle); 
+    li.appendChild(divWhy); 
+    listGroup.appendChild(li); 
 }
