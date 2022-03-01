05415f6020a26bf5b461ad597c6aef6d144f8ddc
cdevelopment010 <cdevelopment010@gmail.com>
2022-03-01 13:13:22 +0000

    Fix: improve timer and stop side effects
    The timer was lagging slight, and this update fixes that for
    the most part. There was a side effect of the timer continuing
    onto the next screen, this has also been fixed. However, there is a
    slight delay.

--- Week22/react/simple-exercises/src/components/timer.js
+++ Week22/react/simple-exercises/src/components/timer.js
@@ -1,19 +1,20 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useRef} from "react";
 
 
-const Timer = () => {
+const  Timer = () => {
 
+    let runTimer = useRef(0); 
+    let interval = useRef(); 
 
-    let audio = new Audio('mixkit-classic-short-alarm-993.wav')
-
-    const zeroPad = (num) => {
-         return String(num).length < 2 ? "0" + num : num; 
-    }
-
-    const updateTimer = (work,count,timeSec) => {
+    const startTimer =  (work,count,timeSec) => {
+        if (runTimer.current === 0) {
+            resetBody();
+            return; 
+        }
+        let audio = new Audio('mixkit-classic-short-alarm-993.wav');        
         audio.play(); 
         document.getElementById('loop').innerText = count;
-        document.getElementById('work-rest').innerText = work == 1 ? 'Work' : 'Rest';
+        document.getElementById('work-rest').innerText = work === 1 ? 'Work' : 'Rest';
 
         if (work === 1) {
             document.body.classList.add('timer-work');
@@ -23,104 +24,124 @@ const Timer = () => {
             document.body.classList.add('timer-rest');
         }
 
-
-        let time = new Date(); 
-        let futureTime = new Date(time.getTime() + (1000 * timeSec) + 800); 
         
-        
-        let timeOut = setInterval(()=>{
-                time = new Date();
-                let timeRemaining =  Math.floor((futureTime.getTime() - time.getTime())/1000)
-                if (timeRemaining >= 0) {
-                    document.getElementById('time-remaining').innerText = zeroPad(timeRemaining); 
-                }
-                else {
-                    clearInterval(timeOut); 
-                }
-            }, 1000 )
+        let time = new Date();   
+        let futureTime = new Date(time.getTime() + (1000 * timeSec + 100)); 
+        interval.current = setInterval(()=>{
 
-        if (count > 1 && work == 1){
+            if (runTimer.current === 0) {
+                clearInterval(interval.current); 
+                resetBody();
+                return; 
+            }
+            
+            time = new Date();
+            let timeRemaining =  Math.floor((futureTime.getTime() - time.getTime())/1000);         
+            if (timeRemaining < 0) {
+                // cancel timer
+                clearInterval(interval.current)
+            } else {
+                document.getElementById('time-remaining').innerText = zeroPad(timeRemaining); 
+            }}, 1000 )
+
+        
+        if (count > 1 && work === 1){
             setTimeout(()=> {
                 console.log('next loop with rest');
-                clearInterval(timeOut); 
-                updateTimer(0, count, document.getElementById('rest-time').value)
-            }, 1000*timeSec + 1000)
+                clearInterval(interval.current); 
+                startTimer(0, count, document.getElementById('rest-time').value)
+            }, 1000*timeSec)
         } else if (count-1 > 0 ) {
             setTimeout(function(){
                 console.log('next loop with work');
-                clearInterval(timeOut); 
-                updateTimer(1,--count, document.getElementById('work-time').value)
-            }, 1000 * timeSec + 1000)
+                clearInterval(interval.current); 
+                startTimer(1,--count, document.getElementById('work-time').value)
+            }, 1000 * timeSec)
         } else {
             setTimeout(function(){
                 console.log('finished');
-                clearInterval(timeOut); 
-                document.getElementById('work-rest').innerText = 'Finished'
-                document.getElementById('loop').innerText = '0'; 
-                document.getElementById('time-remaining').innerText = '0'; 
-                document.body.classList.remove('timer-work');
-                document.body.classList.remove('timer-rest');
+                clearInterval(interval.current); 
+                resetBody();
                 audio.play(); 
                 setTimeout(() => {
                     audio.pause(); 
                     audio.currentTime=0;
-                     audio.play()
+                    audio.play()
                 }, 500)
-            }, 1000 * timeSec + 500)
+            }, 1000 * timeSec)
+        }       
+    }
+
+
+
+    const handleClick = () => {
+        runTimer.current = 1; 
+        startTimer(1,document.getElementById('num-exercises').value,document.getElementById('work-time').value)  
+    }
+
+    const cancelTimer = () => {
+        clearInterval(interval.current); 
+        runTimer.current = 0; 
+    }
+
+    function zeroPad(num) {
+        return String(num).length < 2 ? "0" + num : num; 
+    }
+
+    function resetBody() {
+        document.getElementById('work-rest').innerText = 'Work'
+        document.getElementById('loop').innerText = '0'; 
+        document.getElementById('time-remaining').innerText = '0'; 
+        document.body.classList.remove('timer-work');
+        document.body.classList.remove('timer-rest');
+    }
+
+    useEffect(()=> {
+        return() => {
+            runTimer.current = 0; 
         }
-        
-    }
-
+    })
     
-    const handleClick = ()=> {
-        
-        updateTimer(1,document.getElementById('num-exercises').value,document.getElementById('work-time').value); 
-        
-        
+    return (
+            <div>
+                <div className="timer">
+                <label htmlFor="work-time">
+                    Seconds of exercise
+                    <input type="number" name="work-time" id="work-time" min="0" />
+                </label>
+
+                <label htmlFor="rest-time">
+                    Seconds of Rest
+                    <input type="number" name="rest-time" id="rest-time" min="0"/>
+                </label>
+
+                <label htmlFor="num-exercises">
+                    Number of exercises
+                    <input 
+                        type="number" 
+                        name="num-exercises" 
+                        id="num-exercises" 
+                        min="0"
+                        defaultValue="0"
+                        />
+                </label>
+
+                <button type="button" onClick={() => handleClick()}>START!</button>
+                <button type="button" onClick={cancelTimer}>STOP!</button>
+
+                <div id="work-rest">Work</div>
+                <div>
+                    <div>Time remaining:</div>
+                    <div id="time-remaining">0</div>
+                </div>
+                <div>
+                    <div>Exercises remaining:</div>
+                    <div id="loop">0</div>
+                </div>
+                </div>
+
+            </div>
+        )
     }
-
-   
-
-
-    return(
-        <div>
-            <div className="timer">
-            <label htmlFor="work-time">
-                Seconds of exercise
-                <input type="number" name="work-time" id="work-time" min="0" />
-            </label>
-
-            <label htmlFor="rest-time">
-                Seconds of Rest
-                <input type="number" name="rest-time" id="rest-time" min="0"/>
-            </label>
-
-            <label htmlFor="num-exercises">
-                Number of exercises
-                <input 
-                    type="number" 
-                    name="num-exercises" 
-                    id="num-exercises" 
-                    min="0"
-                    defaultValue="0"
-                    />
-            </label>
-
-            <button type="button" onClick={() => handleClick()}>START!</button>
-
-            <div id="work-rest">Work</div>
-            <div>
-                <div>Time remaining:</div>
-                <div id="time-remaining">0</div>
-            </div>
-            <div>
-                <div>Exercises remaining:</div>
-                <div id="loop">0</div>
-            </div>
-            </div>
-        </div>
-    )
-
-}
 
 export default Timer
